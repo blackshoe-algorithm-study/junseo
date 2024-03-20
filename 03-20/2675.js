1a55ae7e471195fa02b55b9e1b8f16630fd292d8
JUNSEO OH <junseooh@JUNSEOui-MacBookPro.local>
2024-03-20 11:41:46 +0900

    Test: Failed to solve 2675.js

--- 03-20/2675.js
+++ 03-20/2675.js
@@ -20,14 +20,11 @@
 
 const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
-const T = Number(input[0]);
-let result = "";
+const A = parseInt(input[0]);
+const B = input[1];
 
-for (let i = 1; i < input.length; i += 2) {
-  const R = Number(input[i]);
-  const S = input[i + 1];
-  for (let j = 0; j < S.length; j++) {
-    result += S[j].repeat(R);
-  }
-  result += "\n";
-}
+console.log(
+  B?.split("")
+    .map((p) => p.repeat(A))
+    .join("")
+);
