f765220b2455c07ea38ad03f94dd86480a9e63fe
asheroh <wnstj701@dongguk.edu>
2024-04-16 22:59:45 +0900

    ag: Solve 2559

--- 04-15/2559.js
+++ 04-15/2559.js
@@ -1,14 +1,22 @@
+//
 let input = require("fs").readFileSync(0).toString().trim().split("\n");
 
 let [N, K] = input[0].split(" ").map(Number);
 let arr = input[1].split(" ").map(Number);
 
-function solution(N, K, arr) {
-  let answer = 0;
+function solution(totalDays, linkedDays, arr) {
+  let result = [];
 
-  console.log(arr);
+  for (let i = 0; i < totalDays; i++) {
+    if (arr[i + linkedDays - 1] === undefined) break;
+    let sum = 0;
+    for (let j = i; j < i + linkedDays; j++) {
+      sum += arr[j];
+    }
+    result.push(sum);
+  }
 
-  console.log(answer);
+  console.log(Math.max(...result));
 }
 
 solution(N, K, arr);
