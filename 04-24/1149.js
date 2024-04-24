fe611c1352d4a711a35e88e0fadef04ad421bdb3
asheroh <wnstj701@dongguk.edu>
2024-04-24 11:12:50 +0900

    ag: Failed to solve 1149

--- 04-24/1149.js
+++ 04-24/1149.js
@@ -1,1 +1,18 @@
 //
+let [N, ...arr] = require("fs").readFileSync(0).toString().trim().split("\n");
+
+arr = arr.map((e) => e.split(" ").map(Number));
+
+// 빨강 - 초록 - 파랑
+
+function solution(N, arr) {
+  let min = 0;
+
+  arr.forEach((e) => {
+    let red = e[0];
+    let green = e[1];
+    let blue = e[2];
+  });
+}
+
+solution(+N, arr);
