872e5534c791731d28d1af08c27d383d3517183c
asheroh <wnstj701@dongguk.edu>
2024-04-24 11:16:04 +0900

    ag: fix to 1463 solution

--- 04-24/1463.js
+++ 04-24/1463.js
@@ -2,22 +2,22 @@
 const X = +require("fs").readFileSync(0).toString();
 
 // 1 연산 최솟값 = 0
-function dp(X, memo = [0, 0]) {
+function solution(X, dp = [0, 0]) {
   let i = 2;
   while (i <= X) {
     // -1 연산 시 이전 값보다 연산 1회 추가
-    memo[i] = memo[i - 1] + 1;
+    dp[i] = dp[i - 1] + 1;
     if (i % 3 === 0) {
       // -1 연산과 %3 연산 시 횟수 비교
-      memo[i] = Math.min(memo[i], memo[i / 3] + 1);
+      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
     }
     if (i % 2 === 0) {
       // -1 연산과 %2 연산 시 횟수 비교
-      memo[i] = Math.min(memo[i], memo[i / 2] + 1);
+      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
     }
     i++;
   }
-  return memo[X];
+  return dp[X];
 }
 
-console.log(dp(X));
+console.log(solution(X));
