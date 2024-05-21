cd0e54abb055e167e835eb9a1068e734726da59a
asheroh <wnstj701@dongguk.edu>
2024-05-21 20:07:09 +0900

    ag: Solve 7576 re

--- 04-29/7576.js
+++ 04-29/7576.js
@@ -1,33 +1,32 @@
 //
-let input = require("fs").readFileSync(0).toString().trim().split("\n");
+let [MN, ...boxes] = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n");
 
-const ds = [
+const offset = [
   [-1, 0], // 위로 이동
   [1, 0], // 아래로 이동
   [0, 1], // 오른쪽으로 이동
   [0, -1], // 왼쪽으로 이동
 ];
-const [M, N] = input[0].split(" ").map(Number);
+const [M, N] = MN.split(" ").map(Number);
 let queue = [];
-let visit = [...Array(N)].map((e) => Array(M).fill(0));
+let visit = [...Array(N)].map(() => Array(M).fill(0));
 let count = M * N;
 let answer;
 
 // 초기 상태 세팅
-for (let i = 1; i < input.length; i++) {
-  let box = input[i].split(" ").map(Number);
+for (let i = 0; i < N; i++) {
+  let box = boxes[i].split(" ").map(Number);
   box.forEach((tomato, idx) => {
     if (tomato === 1) {
-      // 익은 토마토의 위치와 날짜(0) 큐에 추가하고, 방문 여부 표시
-      queue.push([i - 1, idx, 0]);
-      visit[i - 1][idx] = 1;
-
-      // 미익은 토마토 개수를 줄입니다.
+      queue.push([i, idx, 0]);
+      visit[i][idx] = 1;
       count--;
     } else if (tomato === -1) {
-      // 토마토가 들어있지 않은 칸의 경우 방문 여부 표시
-      visit[i - 1][idx] = 1;
-      // 미익은 토마토 개수를 줄입니다.
+      visit[i][idx] = 1;
       count--;
     }
   });
@@ -36,19 +35,18 @@ for (let i = 1; i < input.length; i++) {
 let idx = 0;
 while (queue.length != idx) {
   const [x, y, day] = queue[idx];
+
   for (let i = 0; i < 4; i++) {
-    const xPos = x + ds[i][0];
-    const yPos = y + ds[i][1];
-    if (xPos < 0 || yPos < 0 || xPos >= N || yPos >= M) continue;
-    if (!visit[xPos][yPos]) {
-      visit[xPos][yPos] = 1;
-      queue.push([xPos, yPos, day + 1]);
+    const nx = x + offset[i][0];
+    const ny = y + offset[i][1];
+    if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+    if (!visit[nx][ny]) {
+      visit[nx][ny] = 1;
+      queue.push([nx, ny, day + 1]);
       count--;
     }
   }
-
   idx++;
   answer = day;
 }
-
 console.log(count ? -1 : answer);
