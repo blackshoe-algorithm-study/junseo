d7ae6752b2ef18f0d103d3b18d10c8e15b199a51
asheroh <wnstj701@dongguk.edu>
2024-05-27 11:13:38 +0900

    ag: Submit 05-27

--- 05-27/2512.js
+++ 05-27/2512.js
@@ -1,1 +1,20 @@
 //
+let [N, arr, M] = require("fs").readFileSync(0).toString().split("\n");
+arr = arr
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+function solution(N, arr, M) {
+  let low = 1;
+  let high = arr[N - 1];
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2); // 현재 상한액
+    const sum = arr.reduce((a, c) => a + (c <= mid ? c : mid));
+    if (sum <= M) low = mid + 1;
+    else high = mid - 1;
+  }
+  console.log(high);
+}
+
+solution(+N, arr, +M);
