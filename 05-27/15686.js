0f76064cf083a0d015f7d25033e19dae4b8c17ec
asheroh <wnstj701@dongguk.edu>
2024-07-14 15:49:08 +0900

    ag: Solve 15686 ref backtracking star

--- 05-27/15686.js
+++ 05-27/15686.js
@@ -1,1 +1,56 @@
 //
+const fs = require("fs");
+let [NM, ...cities] = fs.readFileSync(0).toString().trim().split("\n");
+
+const [N, M] = NM.split(" ").map(Number);
+cities = cities.map((e) => e.split(" ").map(Number));
+
+function getCombinations(arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((value) => [value]);
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((combination) => [fixed, ...combination]);
+    results.push(...attached);
+  });
+  return results;
+}
+
+function calculateChickenDistance(houses, chickenHouses) {
+  let totalDistance = 0;
+  houses.forEach(([hx, hy]) => {
+    let minDistance = Infinity;
+    chickenHouses.forEach(([cx, cy]) => {
+      const distance = Math.abs(hx - cx) + Math.abs(hy - cy);
+      if (distance < minDistance) minDistance = distance;
+    });
+    totalDistance += minDistance;
+  });
+  return totalDistance;
+}
+
+function solution() {
+  let houses = [];
+  let chickenHouses = [];
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (cities[i][j] === 1) houses.push([i, j]);
+      else if (cities[i][j] === 2) chickenHouses.push([i, j]);
+    }
+  }
+
+  const chickenCombinations = getCombinations(chickenHouses, M);
+  let minChickenDistance = Infinity;
+
+  chickenCombinations.forEach((combination) => {
+    const chickenDistance = calculateChickenDistance(houses, combination);
+    if (chickenDistance < minChickenDistance)
+      minChickenDistance = chickenDistance;
+  });
+
+  console.log(minChickenDistance);
+}
+
+solution();
--- 05-27/2512.js
+++ 05-27/2512.js
@@ -9,7 +9,7 @@ function solution(N, arr, M) {
   let low = 1;
   let high = arr[N - 1];
   while (low <= high) {
-    const mid = Math.floor((low + high) / 2); // 현재 상한액
+    const mid = Math.floor((low + high) / 2); // 초기 상한액
     const sum = arr.reduce((a, c) => a + (c <= mid ? c : mid), 0);
     if (sum <= M) low = mid + 1;
     else high = mid - 1;
