9fb55cf3891c52704099c951d29559a046920c92
asheroh <wnstj701@dongguk.edu>
2024-09-19 18:16:52 +0900

    ag: Solve 1956 floyd warshall

--- 07-24/1956.js
+++ 07-24/1956.js
@@ -1,1 +1,34 @@
 //
+let [VE, ...input] = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n");
+const [V, E] = VE.split(" ").map(Number);
+input = input.map((e) => e.split(" ").map(Number));
+
+const dist = Array.from({ length: V }, () => Array(V).fill(Infinity));
+
+input.forEach(([a, b, c]) => {
+  a -= 1;
+  b -= 1;
+  dist[a][b] = Math.min(dist[a][b], c);
+});
+
+for (let k = 0; k < V; k++) {
+  for (let i = 0; i < V; i++) {
+    for (let j = 0; j < V; j++) {
+      if (dist[i][k] !== Infinity && dist[k][j] !== Infinity) {
+        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+      }
+    }
+  }
+}
+
+let answer = Infinity;
+
+for (let i = 0; i < V; i++) {
+  if (dist[i][i] !== Infinity) answer = Math.min(answer, dist[i][i]);
+}
+
+console.log(answer === Infinity ? -1 : answer);
