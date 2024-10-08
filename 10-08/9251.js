197732a83bbbc5c59a30d575260352837f8505ed
asheroh <wnstj701@dongguk.edu>
2024-10-08 11:56:03 +0900

    ag: Solve 9251 ref star dp

--- 10-08/9251.js
+++ 10-08/9251.js
@@ -1,6 +1,28 @@
 //
-let [str1, str2] = require("fs").readFileSync(0).toString().split("\n");
-str1 = str1.split("");
-str2 = str2.split("");
+const [str1, str2] = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n");
 
-const dp = Array.from({ length: Math.min(str1.length, str2.length) }, () => []);
+const len1 = str1.length;
+const len2 = str2.length;
+
+// DP 테이블 생성 (문자열 길이에 맞춰 2차원 배열)
+const dp = Array.from({ length: len1 + 1 }, () => Array(len2 + 1).fill(0));
+
+// DP 계산
+for (let i = 1; i <= len1; i++) {
+  for (let j = 1; j <= len2; j++) {
+    if (str1[i - 1] === str2[j - 1]) {
+      // 문자가 같으면, 이전 값에서 +1
+      dp[i][j] = dp[i - 1][j - 1] + 1;
+    } else {
+      // 문자가 다르면, 왼쪽 또는 위쪽 값 중 큰 값을 취함
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+}
+
+// 최종 LCS 길이 출력
+console.log(dp[len1][len2]);
